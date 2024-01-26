1b09f72cf810279e13fc235c02a0782b1829347f
asyasuri1991 <asyasuri@gmail.com>
2024-01-26 09:58:42 +0300

    add

---
+++ js/header.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+
+
+    var header = document.querySelector('header');
+    
+    
+    header.classList.toggle('fixed', window.scrollY > 0);
+    
+    
+    });
+    
+    
