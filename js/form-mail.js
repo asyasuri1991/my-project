8b8ca209823f92def755d4b67da6c7929d82c837
asyasuri1991 <asyasuri@gmail.com>
2024-02-07 13:53:41 +0300

    change

--- js/form-mail.js
+++
@@ -1,6 +0,0 @@
-var email = document.getElementById("email").value;
-
-
-var emailContent = "Email: " + email;
-window.open("mailto:asya.suri@yandex.ru?subject=Новое сообщение&body=" + emailContent);
-
