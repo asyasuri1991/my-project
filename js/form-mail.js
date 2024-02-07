1ef88b0ca8c74a9f4694b195a2f41a862fe12e9d
asyasuri1991 <asyasuri@gmail.com>
2024-02-07 13:37:26 +0300

    upd js

---
+++ js/form-mail.js
@@ -0,0 +1,6 @@
+var email = document.getElementById("email").value;
+
+
+var emailContent = "Email: " + email;
+window.open("mailto:asya.suri@yandex.ru?subject=Новое сообщение&body=" + emailContent);
+
