eaae8e67116ed59dcc6dcca890c53b612f31ef7b
Kirill <k187m18733@gmail.com>
2024-02-08 09:53:11 +0300

    feat: add adaptive for main page

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,13 +1,30 @@
-var swiper = new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 40,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+    991: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    320: {
+      slidesPerView: 1
+    },
+  },
 });
